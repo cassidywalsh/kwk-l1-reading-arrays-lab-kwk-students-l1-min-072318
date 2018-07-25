e5cb6d5d5bdcde0c0dc2f9d2b9f8a70bc5be196e
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 14:59:27 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,6 +11,8 @@ def first_student_by_index
   puts STUDENT_NAMES[0]
 end
 
+first_student_by_index
+
 # def fourth_student_by_index
 #   # Write a solution that returns the first student in the array student_names
 # end
