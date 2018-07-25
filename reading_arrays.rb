a4c2023f4b23f4d362f4f91556c3822e57873606
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 15:00:23 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,15 +7,17 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-def first_student_by_index
-  puts STUDENT_NAMES[0]
+# def first_student_by_index
+#   puts STUDENT_NAMES[0]
+# end
+
+# first_student_by_index
+
+def fourth_student_by_index
+  puts STUDENT_NAMES[3]
 end
 
-first_student_by_index
-
-# def fourth_student_by_index
-#   # Write a solution that returns the first student in the array student_names
-# end
+fourth_student_by_index
 
 # def last_student_by_index
 #   # Write a solution that returns the first student in the array student_names
