626afbff04491a641c682fbf6eb78daed25086c6
cassidywalsh <cassidy.walsh19@blstudent.org>
2018-07-25 15:08:32 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -14,31 +14,31 @@ end
 first_student_by_index
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+  return STUDENT_NAMES[3]
 end
 
 fourth_student_by_index
 
 def last_student_by_index
-  puts STUDENT_NAMES[5]
+  return STUDENT_NAMES[5]
 end
 
 last_student_by_index
 
 def first_student_by_method
-  puts STUDENT_NAMES.first
+  return STUDENT_NAMES.first
 end
 
 first_student_by_method
 
 def last_student_by_method
-  puts STUDENT_NAMES.last
+  return STUDENT_NAMES.last
 end
 
 last_student_by_method
 
 def first_second_and_third_students
-  puts STUDENT_NAMES[0..2]
+  return STUDENT_NAMES[0..2]
 end
 
 first_second_and_third_students
